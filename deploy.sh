65a13e6b6b841c12ef90e82088b0ca8df696311f
V3XL <48788566+V3XL@users.noreply.github.com>
2025-03-23 15:32:39 +1100

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+cp cpu_power_saver.sh /usr/local/bin/
+chmod +x /usr/local/bin/cpu_power_saver.sh
+
+SERVICE_FILE="/etc/systemd/system/cpu_power_saver.service"
+
+echo "[Unit]
+Description=CPU Power Saver Service
+After=network.target
+
+[Service]
+ExecStart=/usr/local/bin/cpu_power_saver.sh
+Restart=always
+User=root
+Group=root
+Environment=PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+Environment=HOME=/root
+
+[Install]
+WantedBy=multi-user.target" > $SERVICE_FILE
+
+systemctl daemon-reload
+systemctl start cpu_power_saver.service
+systemctl enable cpu_power_saver.service
